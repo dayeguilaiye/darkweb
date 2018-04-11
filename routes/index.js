fe14d961ed9692aa3af73661b030bff08b02ce4c
dayeguilaiye <979014041@qq.com>
2018-04-11 19:38:31 +0800

    完全跑通服务器-数据库-前端的数据传输过程

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,39 @@
 var express = require('express');
 var router = express.Router();
-var mysql      = require('mysql');
+var mysql = require('mysql');
+var query = require("../pool/mysqlPool");
+
+query("select * from table where id=?", [1], function (err, results, fields) {
+  //do something  
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/upload', function(req, res, next) {
+router.get('/upload', function (req, res, next) {
   res.render("upload");
 });
 
-router.get('/cameras', function(req, res, next) {
-  // var connection = mysql.createConnection({
-  //   host     : 'www.hitnrun.cn',
-  //   user     : 'root',
-  //   password : '404',
-  //   port     : '3306',
-  //   database : 'darkweb'
-  // });
-  
-  // connection.connect();
-  
-  // connection.query('SHOW DATABASES;', function (error, results, fields) {
-  //   if (error) throw error;
-  //   console.log('The solution is: ', results[0].solution);
-  // });
+router.get('/cameras', function (req, res, next) {
 
-
-  var cameras = [
-    {"name":"东门","ip":"192.168.233.233","status":"完全毁坏"},
-    {"name":"北门","ip":"192.168.404.404","status":"好的不行"},
-    {"name":"南门","ip":"192.168.40.40","status":"被遮盖"},
-    {"name":"机密要地","ip":"404.404.404.404","status":"极度清晰"}
-  ]
-  res.send(cameras);
+  query("select * from camera", [1], function (err, results, fields) {
+    var cameras = [];
+    console.log(results);
+    for (var i = 0; i < results.length; i++) {
+      var singleResult = results[i];
+      cameras[i] = { "name": singleResult['name'], "ip": singleResult['ip'], "status": singleResult['status'] }
+    }
+    console.log(cameras);
+    res.send(cameras);
+  });
+  // var cameras = [
+  //   { "name": "东门", "ip": "192.168.233.233", "status": "完全毁坏" },
+  //   { "name": "北门", "ip": "192.168.404.404", "status": "好的不行" },
+  //   { "name": "南门", "ip": "192.168.40.40", "status": "被遮盖" },
+  //   { "name": "机密要地", "ip": "404.404.404.404", "status": "极度清晰" }
+  // ]
 });
 
 // app.post('/uploadPic', function (req, res) {
